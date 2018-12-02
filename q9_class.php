6941c850097acb5906262458e8e6452c75728684
gowthamk <gkaki@purdue.edu>
2018-12-02 23:21:53 +0000

    Screencast link added

--- q9_class.php
+++ q9_class.php
@@ -33,6 +33,7 @@ class Q9 implements MessageComponentInterface {
     $obj = json_decode($msg);
     $rand_str = $this->generateRandomString();
     $header = ($obj->app)."_app_header.ml";
+    $k = ($obj->app == "ba")? 5 : 6;
     $app = ($obj->app)."_".($rand_str)."_app.ml";
     shell_exec('cp '.$header.' '.$app);
     $handle = fopen($app, 'a') or die('Cannot open file:  '.$app);
@@ -40,7 +41,7 @@ class Q9 implements MessageComponentInterface {
     fclose($handle);
     /* execute q9 */
     echo("File generated\n");
-    $cmd = './q6 -k 4 -c store_interface.mli store_interface.ml uuid.mli uuid.ml q6_interface.ml '.$app;
+    $cmd = './q6 -k '.$k.' -c store_interface.mli store_interface.ml uuid.mli uuid.ml q6_interface.ml '.$app;
     flush();
     $stream = new Stream(popen($cmd, "r"),$this->loop);
     $stream->on('data', function($data) use ($conn) {
@@ -51,17 +52,6 @@ class Q9 implements MessageComponentInterface {
       $conn->send("QUIT");
 			echo 'END';
 		});
-    /*if (is_resource($handle)) {
-        echo("Running..\n");
-        while ($s = fgets($handle)) {
-            $conn->send($s);
-            echo($s);
-        }
-    }
-    else {
-        echo("Command couldn't run\n");
-    }
-    $conn->send("QUIT");*/
   }
 
   public function onClose(ConnectionInterface $conn) {
